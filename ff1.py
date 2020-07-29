ba44c4bd9ef568ed795c292c01787f669b679be6
volkswind <volkswind@gmail.com>
2020-07-29 16:12:16 +0100

    omit the empty row and reduce some useless columns like status code and raw response

--- ff1.py
+++ ff1.py
@@ -12,73 +12,73 @@ def scraper(url):
     urlid = re.findall('[0-9]{11,}', url)[0]
     base_url = 'https://www.autotrader.co.uk/json/fpa/initial/'
     response = requests.get(base_url + str(urlid), timeout=5)
-    
+
     ret = dict()
-    
+
     ret['url'] = url
-    ret['status_code'] = response.status_code
+    #ret['status_code'] = response.status_code
     if response.status_code != 200:
         return ret
-    
-    ret['raw_response'] = response.content.decode('utf-8')
-    
+
+    #ret['raw_response'] = response.content.decode('utf-8')
+
     d = json.loads(response.content.decode('utf-8'))
-    
+
     keys_vehicle = {
         'make', 'model', 'trim', 'condition', 'tax', 'co2Emissions'
     }
     for nm in set(d['vehicle'].keys()).intersection(keys_vehicle):
         ret[nm] = d['vehicle'][nm]
-        
+
     keys_keyFacts = {
-        'engine-size', 'manufactured-year', 'body-type', 'mileage', 
+        'engine-size', 'manufactured-year', 'body-type', 'mileage',
         'transmission', 'fuel-type', 'doors', 'seats'
     }
     for nm in set(d['vehicle']['keyFacts'].keys()).intersection(keys_keyFacts):
         ret[nm] = d['vehicle']['keyFacts'][nm]
-        
+
     if 'doors' in ret.keys():
         match = re.search('\d+', ret['doors'])
         if match:
             ret['doors'] = match[0]
-    
+
     if 'seats' in ret.keys():
         match = re.search('\d+', ret['seats'])
         if match:
             ret['seats'] = match[0]
-    
+
     if 'manufactured-year' in ret.keys():
         match = re.search('\d{4}', ret['manufactured-year'])
         if match:
             ret['manufactured-year'] = match[0]
-            
+
     if 'mileage' in ret.keys():
         ret['mileage'] = re.sub('[^\d\.]', '', ret['mileage'])
-    
+
     if 'co2Emissions' in ret.keys():
         ret['co2Emissions'] = re.sub('[^\d\.]', '', ret['co2Emissions'])
-    
-    
+
+
     keys_advert = {'price', 'description'}
     for nm in set(d['advert'].keys()).intersection(keys_advert):
         ret[nm] = d['advert'][nm]
-        
+
     if 'price' in ret.keys():
         ret['price'] = re.sub('[^\d]', '', ret['price'])
-    
+
     keys_seller = {'isTradeSeller', 'townAndDistance', 'emailAddress'}
     for nm in set(d['seller'].keys()).intersection(keys_seller):
         ret[nm] = d['seller'][nm]
-        
+
     keys_tracking = {
         'average_mpg', 'vehicle_check_status'
     }
     for nm in set(d['pageData']['tracking'].keys()).intersection(keys_seller):
         ret[nm] = d['pageData']['tracking'][nm]
-        
+
     for nm in list(ret.keys()):
         ret[re.sub('-', '_', nm)] = ret.pop(nm)
-                   
+
     return ret
 
 
@@ -98,8 +98,8 @@ def search_result_scraper(url):
     tree = html.fromstring(response.content.decode('utf-8'))
     res = set(tree.xpath('//a[contains(@class, "listing-fpa-link")]/@href'))
     return(list(res))
-    
-    
+
+
 
 def c_scraper(url):
     tree = tree_getter(url)
@@ -109,8 +109,8 @@ def c_scraper(url):
         print()
         car_count = int(
             re.sub(
-                '[^0-9]', 
-                '', 
+                '[^0-9]',
+                '',
                 tree.xpath('//h1[@class="search-form__count js-results-count"]/text()')[0]
             )
         )
--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@
 # Will take a list of autotrader ad page IDs and scrape useful data from them,
 # outputting in .csv format. The scraper function itself can be found in ff1.py
 
-# Takes three arugments, in order: 
+# Takes three arugments, in order:
 # 1. an input file name (one line per ad url)
 # 2. an output file name (appends by default)
 
@@ -13,30 +13,35 @@ import csv
 import os
 
 import ff1
-    
-with open(str(sys.argv[1]), 'r') as i_file: # url file 
+print ("This is the name of the output target: ", sys.argv[2])
+
+with open(str(sys.argv[1]), 'r') as i_file: # url file
     url_list = i_file.readlines()
 
 if os.path.isfile(sys.argv[2]):
-    mode = 'a'
+    mode = 'a' #if there is such a file, append it (write from the end)
 else:
-    mode = 'w'
-    
-with open(str(sys.argv[2]), mode) as o_file: 
+    mode = 'w' #if there isn't such a file, create it (write from the start)
+
+with open(str(sys.argv[2]), mode, encoding = 'gb18030', newline='') as o_file:
     i = 0
     field_names = [
-        'url', 'status_code', 'make', 'model', 'trim', 'manufactured_year', 
-        'condition', 'transmission', 'body_type', 'doors', 
-        'engine_size', 'seats', 'fuel_type', 'description', 'price', 
-        'townAndDistance', 'isTradeSeller', 'emailAddress', 'tax', 
-        'co2Emissions', 'mileage', 'raw_response'
+        'url', 'make', 'model', 'trim', 'manufactured_year',
+        'condition', 'transmission', 'body_type', 'doors',
+        'engine_size', 'seats', 'fuel_type', 'description', 'price',
+        'townAndDistance', 'isTradeSeller', 'emailAddress', 'tax',
+        'co2Emissions', 'mileage'
     ]
+    print('writing the titles...')
     cwriter = csv.DictWriter(o_file, fieldnames=field_names)
-    
+
     if mode == 'w':
+        print('we are in w mode')
         cwriter.writeheader()
     for u in url_list:
         tmp = ff1.scraper(u)
+        print(sys.getdefaultencoding())
+        #print('This will be written to the csv file:',tmp)
         cwriter.writerow(tmp)
         i+=1
         if i % 10 == 0:
